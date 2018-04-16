552d22fdfab19b71d085aef12911738287c7288d
demeiyan_dell <1312480794@qq.com>
2018-04-16 09:38:45 +0800

    debug

--- logistic regression/LR_main.py
+++ logistic regression/LR_main.py
@@ -12,8 +12,12 @@ def smote():
     pass
 
 
-def sigmoid(x, belta):
-    return np.exp(np.dot(x, belta))/(1+np.exp(np.dot(x, belta)))
+def sigmoid(belta, x):
+    try:
+        return np.exp(np.dot(belta, x))/(1+np.exp(np.dot(belta, x)))
+    except Exception as insit:
+        print(insit)
+        print(belta, x, np.dot(belta, x))
 
 
 def convertdata(feature, label):
@@ -31,9 +35,9 @@ def convertdata(feature, label):
     return x, y
 
 
-def binaryclassification(x, y, one, belta,learn_rate):
-    y = np.array([i & one for i in y])
-    der = y - sigmoid(x, belta)
+def binaryclassification(x, y, one, belta, learn_rate):
+    y = np.array([int(i == one) for i in y])
+    der = sigmoid(belta, x.T) - y # sigmoid(belta, x.T) - y
     der = np.reshape(der, (y.shape[0], 1))
     der = np.sum(x*der, axis=0)
     belta = belta - learn_rate*der
@@ -41,28 +45,34 @@ def binaryclassification(x, y, one, belta,learn_rate):
 
 
 if __name__ == '__main__':
-    min_batch = 15
+    min_batch = 64
     learn_rate = 0.001
     x_train, label_train = convertdata('page_blocks_train_feature.txt', 'page_blocks_train_label.txt')
     x_test, label_test = convertdata('page_blocks_test_feature.txt', 'page_blocks_test_label.txt')
-    # 归一化,对特征的每一个维度计算x = (x-mean)/std归一化到正态分布
+    # 归一化,对特征的每一个维度计算x = (x-mean)/std 归一化到正态分布
     std = x_train.std(axis=0)
     mean = x_train.mean(axis=0)
     x_train = (x_train - mean)/std
     x_test = (x_test - mean)/std
-    # print(mean, x_train)
+    # print(x_test)
     x_train = np.concatenate((x_train, np.ones((1, x_train.shape[0])).T), axis=1)
     x_test = np.concatenate((x_test, np.ones((1, x_test.shape[0])).T), axis=1)
     label_train = np.array([int(x) for x in label_train])
     label_test = np.array([int(x) for x in label_test])
     datasize = label_train.shape[0]
-    belta = np.zeros((5, 11))
-    #binaryclassification(x_train[0:64], label_train[0:64], 1, belta[1], learn_rate)
-    # for epoch in range(10)
-    for i in range(5):
-        for j in range(int(datasize/min_batch)):
-            belta[i] = binaryclassification(x_train[j*min_batch:(j+1)*min_batch], label_train[j*min_batch:(j+1)*min_batch], i+1, belta[i], learn_rate)
-    print(belta)
+    belta = np.random.random((5, 11))
+    # belta[0] = binaryclassification(x_train[0 * min_batch:(0 + 1) * min_batch],label_train[0 * min_batch:(0 + 1) * min_batch], 5, belta[0], learn_rate)
 
-    # print(np.shape(x_train[0:min_batch]))
-    # print(np.shape(label_train))
+    # for epoch in range(100):
+    #     print(belta[0])
+    #     for i in range(int(datasize/min_batch)):
+    #         belta[0] = binaryclassification(x_train[i*min_batch:(i+1)*min_batch], label_train[i*min_batch:(i+1)*min_batch], 5, belta[0], learn_rate)
+
+    for epoch in range(100):
+        for i in range(5):
+            for j in range(int(datasize/min_batch)):
+                belta[i] = binaryclassification(x_train[j*min_batch:(j+1)*min_batch], label_train[j*min_batch:(j+1)*min_batch], i+1, belta[i], learn_rate)
+        #print(belta)
+        predict = sigmoid(belta, x_test.T)
+        predict = np.argmax(predict, axis=0) + 1
+        print('epoch %d %.2f%%' % (epoch, (predict == label_test).sum()/label_test.shape[0]*100))
